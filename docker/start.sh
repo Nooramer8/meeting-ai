ffca4ffa1f70b6024101efe2b7ee253c8fdd09e7
noor <nooramer19958@gmail.com>
2026-05-18 12:39:35 +0300

    Seed default users on deploy

--- docker/start.sh
+++ docker/start.sh
@@ -7,4 +7,8 @@ if [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
     php artisan migrate --force
 fi
 
+if [ "${RUN_SEEDERS:-true}" = "true" ]; then
+    php artisan db:seed --force
+fi
+
 exec php artisan serve --host=0.0.0.0 --port="${PORT:-8000}"
